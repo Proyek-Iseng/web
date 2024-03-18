b4cfcc04b295b4ee5148ec559e269765d5e24561
SulthonAbady17 <sulthon.abady28@gmail.com>
2024-03-19 00:59:14 +0700

    setup admin model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            // 'phone' => fake('id')->mobileNumber(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
